797660d38dd6623184620dca61366a2eb1e78cfa
RaflieModz <yonoandi814@gmail.com>
2025-04-18 22:23:35 +0700

    Create raflie.js

---
+++ raflie.js
@@ -0,0 +1,17 @@
+{
+
+  "secretCode": "jomok",
+
+  "description": "EvilCrasher", 
+
+  "lastUpdated": "-",
+
+  "author": "Danzy", 
+
+  "status": "Active",
+
+  "license": "MIT",
+
+  "version": "3.0"
+
+}
